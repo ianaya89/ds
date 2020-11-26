0f3088b2a1860be6297d743f43922c30e8a14df5
Nacho Anaya <ignacio.anaya89@gmail.com>
2020-11-26 18:37:52 -0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,9 +1,11 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve({ port: 8000 });
+import { serve } from "https://deno.land/std@0.79.0/http/server.ts";
 
-console.log("http://localhost:8000/");
+const server = serve({ hostname: "0.0.0.0", port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
 
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" });
+for await (const request of server) {
+  let bodyContent = "Your user-agent is:\n\n";
+  bodyContent += request.headers.get("user-agent") || "Unknown";
+
+  request.respond({ status: 200, body: bodyContent });
 }
-
